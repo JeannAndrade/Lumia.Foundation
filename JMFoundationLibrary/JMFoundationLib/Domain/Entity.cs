cebd483cf60e38b73ddaaf5e2121e8999c4f986c
Jeann Andrade <jeann.andrade@outlook.com>
2023-07-28 20:54:51 -0300

    Adicionado classes básicas de domínio, Entity e ValueObject

---
+++ JMFoundationLibrary/JMFoundationLib/Domain/Entity.cs
@@ -0,0 +1,12 @@
+namespace JMFoundationLib.Domain
+{
+  public abstract class Entity
+  {
+    public Guid Id { get; private set; }
+
+    public Entity()
+    {
+      Id = Guid.NewGuid();
+    }
+  }
+}
